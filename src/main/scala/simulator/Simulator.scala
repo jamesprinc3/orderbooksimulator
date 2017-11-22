ea5fe9a060b969e8b56cb72434e169909586ab2c
James Prince <jamesprinc3@me.com>
2017-11-22 17:11:45 +0000

    Basic simulator coded up

--- src/main/scala/simulator/Simulator.scala
+++ src/main/scala/simulator/Simulator.scala
@@ -1,5 +1,29 @@
 package simulator
 
-class Simulator {
+import java.time.LocalDateTime
+
+import simulator.orderbook.OrderBook
+import simulator.trader.Trader
+
+import scala.concurrent.duration.Duration
+
+// TODO: make this into an interface?
+// TODO: better names for input args
+class Simulator(startTime: LocalDateTime,
+                private val increment: Duration,
+                private val timeSteps: Int,
+                traders: List[Trader],
+                orderBooks: List[OrderBook]) {
+
+  private var elapsedTimeSteps = 0
+  private var time = startTime
+
+  def run(): Unit = {
+    while (elapsedTimeSteps < timeSteps) {
+      time.plusNanos(increment.toNanos)
+      traders.foreach(_.step(time))
+      elapsedTimeSteps += 1
+    }
+  }
 
 }
